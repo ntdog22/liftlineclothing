43ff29a00394a1417b4e73986270a132ea4c6779
Niko Tokuno <niko.tokuno@gmail.com>
2014-03-05 16:20:33 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liftline::Application.config.secret_key_base = '3085c019e613a71083d375b69d9d132781006c0ec3c56c9c7a27aaa1787ebd386849503d3d8a89af557850c3a5104ff122382afb54a7472ca766b5db0da7c298'
