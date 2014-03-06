bc6355090905c7e97e99934b6aadedf4a30b32da
Niko Tokuno <niko.tokuno@gmail.com>
2014-03-05 16:47:53 -0800

    Added Routes and User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Liftline::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
-  get "pages/products"
+  get "users/new"
+  match "/home", to: "pages#home",        via: "get"
+  match "/about", to: "pages#about",      via: "get"
+  match "/contact", to: "pages#contact",  via: "get"
+  match "/products", to: "pages#contact", via: "get"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
