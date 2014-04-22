364c34f5b73c24b15f77f0dd0f80708e87da7137
Niko Tokuno <niko.tokuno@gmail.com>
2014-04-22 16:50:12 -0700

    force refresh pages. contact fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   before_filter :prepare_for_mobile
-
-  private 
-
+  before_filter :set_cache_headers
+private 
   def mobile_device?
   	if session[:mobile_param]
-  		  session[:mobile_param] == "1"
-  	 else
+  		 session[:mobile_param] == "1"
+  	  else
   	   request.user_agent =~ /Mobile|WebOS/
     end
   end
-  helper_method :mobile_device?
-
-
+helper_method :mobile_device?
   def prepare_for_mobile
   	session[:mobile_param] = params[:mobile] if params[:mobile]
   	request.format = :mobile if mobile_device?
   end
+   def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+   end
 end
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,4 @@
 class Notifier < ActionMailer::Base
  
-  def support_notification(sender)
-    @sender = sender
-    mail(:to => "administrator[at]yourdomain.com",
-         :from => sender.email,
-         :subject => "New #{sender.support_type}")
-  end
+
 end
